fa7ded5a90c9964afeb6303cf7351f8851374ae7
Abdulaziz Fakhriddinov <mr_abdulaziz777@list.ru>
2023-06-27 02:32:29 +1000

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -162,3 +162,4 @@ SELECT species.name, COUNT(species.name) FROM vets JOIN visits ON visits.vet_id 
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
 
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
